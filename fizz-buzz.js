b768b77ab41679523f39a716b331f1ec41452c9c
rasantoe <rasantos.dwfc5@ucamp.utel.edu.mx>
2022-03-23 20:02:12 -0400

    Commit-1

---
+++ fizz-buzz.js
@@ -0,0 +1,37 @@
+let numSle = [];
+ 
+for (let i = 1; i <= 100; i++) {
+  numSle.push(i)
+  
+}
+
+console.log(numSle)
+
+for (let i = 2; i <= numSle.length; i++ ) {
+  numSle.fill('fizz' , i, i+1);
+  i++
+  i++
+  
+}
+for (let i = 4; i < numSle.length; i++) {
+  if (numSle[i] == 'fizz') {
+    numSle.fill('fizz-buzz', i, i+1);
+    i++
+    i++
+    i++
+    i++
+  }else{
+    
+    numSle.fill('buzz', i, i+1);
+    i++
+    i++
+    i++
+    i++
+  }
+  
+}
+  
+
+  
+
+document.write(numSle);
